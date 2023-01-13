20b995a8bd6505f26b729e7ff9f15762bc5c9f17
kasys1422 <84632366+kasys1422@users.noreply.github.com>
2023-01-13 16:35:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,9 @@ class ReversiSystem:
         for i in range(self.board_size):
             for j in range(self.board_size):
                 pygame.draw.rect(self.surface, (0, 0, 0), (self.x+i*self.pitch_x,self.y+j*self.pitch_y,self.pitch_x,self.pitch_y), 2)
-                if(self.board[i][j] == 1):
+                if self.board[i][j] == 1:
                     pygame.draw.circle(self.surface, (0, 0, 0), (int(self.x+i*self.pitch_x+(self.pitch_x/2)),int(self.y+j*self.pitch_y+(self.pitch_y/2))), int((self.pitch_x+self.pitch_y)*0.2))
-                elif(self.board[i][j] == 2):
+                elif self.board[i][j] == 2:
                     pygame.draw.circle(self.surface, (255, 255, 255), (int(self.x+i*self.pitch_x+(self.pitch_x/2)),int(self.y+j*self.pitch_y+(self.pitch_y/2))), int((self.pitch_x+self.pitch_y)*0.2))
 
     # クリック場所取得関数
@@ -55,33 +55,33 @@ class ReversiSystem:
     # 石を設置可能な場所を取得
     def get_settable_point(self, id):
         settable_point = [[0] * self.board_size for i in range(self.board_size)]
-        if(not (id == 1 or id == 2)):
+        if not (id == 1 or id == 2):
             return settable_point
         else:
             for i in range(self.board_size):
                 for j in range(self.board_size):
-                    if(self.board[i][j] != 0):
+                    if self.board[i][j] != 0:
                         settable_point[i][j] = 0
                     else:
                         for k in range(-1,2):
                             for l in range(-1,2):
-                                if(not (k == 0 and l == 0)):
+                                if not (k == 0 and l == 0):
                                     n = 0
                                     while 1:
-                                        if (i+k*(n+1) < 0 or j+l*(n+1) < 0 or i+k*(n+1) > self.board_size-1 or j+l*(n+1) > self.board_size-1):
+                                        if i+k*(n+1) < 0 or j+l*(n+1) < 0 or i+k*(n+1) > self.board_size-1 or j+l*(n+1) > self.board_size-1:
                                             break
-                                        if (id == 1):
-                                            if(self.board[i+k*(n+1)][j+l*(n+1)] == 2):
+                                        if id == 1:
+                                            if self.board[i+k*(n+1)][j+l*(n+1)] == 2:
                                                 n = n + 1
-                                            elif(self.board[i+k*(n+1)][j+l*(n+1)] == 1 and n >= 1):
+                                            elif self.board[i+k*(n+1)][j+l*(n+1)] == 1 and n >= 1:
                                                 settable_point[i][j] = 1
                                                 break
                                             else:
                                                 break
-                                        elif (id == 2):
-                                            if(self.board[i+k*(n+1)][j+l*(n+1)] == 1):
+                                        elif id == 2:
+                                            if self.board[i+k*(n+1)][j+l*(n+1)] == 1:
                                                 n = n + 1
-                                            elif(self.board[i+k*(n+1)][j+l*(n+1)] == 2 and n >= 1):
+                                            elif self.board[i+k*(n+1)][j+l*(n+1)] == 2 and n >= 1:
                                                 settable_point[i][j] = 1
                                                 break
                                             else:
@@ -90,7 +90,7 @@ class ReversiSystem:
 
     # 石を設置可能か取得
     def can_set_new_point(self, id):
-        if(self.get_settable_point(id) == [[0] * self.board_size for i in range(self.board_size)]):
+        if self.get_settable_point(id) == [[0] * self.board_size for i in range(self.board_size)]:
             return False
         else:
             return True
@@ -100,8 +100,8 @@ class ReversiSystem:
         board = self.get_settable_point(id)
         for i in range(self.board_size):
             for j in range(self.board_size):
-                if(board[i][j] == 1):
-                    if((i,j) == self.get_mouse_board_area()):
+                if board[i][j] == 1:
+                    if (i,j) == self.get_mouse_board_area():
                         color = color_active
                     else:
                         color = color_negative
@@ -109,30 +109,30 @@ class ReversiSystem:
 
     # 新しい石を設置
     def set_new_point(self, id, x, y):
-        if(not (id == 1 or id == 2)):
+        if not (id == 1 or id == 2):
             return
         else:
-            if(self.board[x][y] == 0):
+            if self.board[x][y] == 0:
                 for k in range(-1,2):
                     for l in range(-1,2):
-                        if(not (k == 0 and l == 0)):
+                        if not (k == 0 and l == 0):
                             n = 0
                             while 1:
-                                if (x+k*(n+1) < 0 or y+l*(n+1) < 0 or x+k*(n+1) > self.board_size-1 or y+l*(n+1) > self.board_size-1):
+                                if x+k*(n+1) < 0 or y+l*(n+1) < 0 or x+k*(n+1) > self.board_size-1 or y+l*(n+1) > self.board_size-1:
                                     break
-                                if (id == 1):
-                                    if(self.board[x+k*(n+1)][y+l*(n+1)] == 2):
+                                if id == 1:
+                                    if self.board[x+k*(n+1)][y+l*(n+1)] == 2:
                                         n = n + 1
-                                    elif(self.board[x+k*(n+1)][y+l*(n+1)] == 1 and n >= 1):
+                                    elif self.board[x+k*(n+1)][y+l*(n+1)] == 1 and n >= 1:
                                         for i in range(n):
                                             self.board[x+k*(i+1)][y+l*(i+1)] = 1
                                         break
                                     else:
                                         break
                                 elif (id == 2):
-                                    if(self.board[x+k*(n+1)][y+l*(n+1)] == 1):
+                                    if self.board[x+k*(n+1)][y+l*(n+1)] == 1:
                                         n = n + 1
-                                    elif(self.board[x+k*(n+1)][y+l*(n+1)] == 2 and n >= 1):
+                                    elif self.board[x+k*(n+1)][y+l*(n+1)] == 2 and n >= 1:
                                         for i in range(n):
                                             self.board[x+k*(i+1)][y+l*(i+1)] = 2
                                         break
@@ -143,17 +143,17 @@ class ReversiSystem:
     #プレイヤーの入力を待機(id=1:黒, id=2:白)
     def standby_player_input(self, events, id):
         res = 0
-        if(self.can_set_new_point(id) == False and self.pass_count < 2):
+        if self.can_set_new_point(id) == False and self.pass_count < 2:
             self.pass_count = self.pass_count + 1
             return 2
         for event in events:
             if event.type == MOUSEBUTTONDOWN:
                 x, y = self.get_mouse_board_area()
-                if(self.get_settable_point(id)[x][y] == 1):
+                if self.get_settable_point(id)[x][y] == 1:
                     self.set_new_point(id, x, y)
                     self.pass_count = 0
                     res = 1
-        if(self.check_end_game() == True):
+        if self.check_end_game() == True:
             res = 3
         return res
 
@@ -167,22 +167,22 @@ class ReversiSystem:
     # コンピューターの入力を待機
     def standby_computer_input(self, id, callback, wait_seconds = 0.5):
         pygame.time.wait(int(wait_seconds * 1000))
-        if(self.can_set_new_point(id) == False and self.pass_count < 2):
+        if self.can_set_new_point(id) == False and self.pass_count < 2:
             self.pass_count = self.pass_count + 1
             return 2
         settable_point = self.get_settable_point(id)
         (x, y) = callback(self, id, self.board, settable_point)
-        if(settable_point[x][y] == 1):
+        if settable_point[x][y] == 1:
             self.set_new_point(id, x, y)
             self.pass_count = 0
-        if(self.check_end_game() == True):
+        if self.check_end_game() == True:
             return 3
         return 1
 
     # ゲーム終了確認
     def check_end_game(self):
         #print(self.count(1) + self.count(2))
-        if(self.pass_count >= 2 or self.count(1) + self.count(2) >= self.board_size ** 2):
+        if self.pass_count >= 2 or self.count(1) + self.count(2) >= self.board_size ** 2:
             return True
         else:
             return False
@@ -235,16 +235,16 @@ def main():
         rs.draw_board()
 
         # パスを確認
-        if(pass_timer > 0):
+        if pass_timer > 0:
             #pygame.time.wait(pass_timer)
             surface.blit(get_pygame_transparent_surface(720, 720, (0, 0, 0, 127)),[0, 0])
             surface.blit(font[2].render("Pass", True, (255,255,255)), [20, 20])
             pass_timer = pass_timer - 1
-        elif(wait_timer > 0):
+        elif wait_timer > 0:
             wait_timer = wait_timer - 1
         else:
             # スタート画面
-            if(event_state == 0):
+            if event_state == 0:
                 # 半透明背景
                 surface.blit(get_pygame_transparent_surface(720, 720, (0, 0, 0, 127)),[0, 0])
                 # 文字列
@@ -268,49 +268,49 @@ def main():
                             player2 = "computer"
 
             # プレイヤー1のターン
-            elif(event_state == 1):
-                if(player1 == "player"):
+            elif event_state == 1:
+                if player1 == "player":
                     rs.draw_settable_point(1)
                     input_state = rs.standby_player_input(events, 1)
                 else:
                     input_state = rs.standby_computer_input(1, computer_test_player, 0)
-                if(input_state == 1 or input_state == 2):
-                    if(input_state == 2 and (player1 == "player" or player2 == "player")):
+                if input_state == 1 or input_state == 2:
+                    if input_state == 2 and (player1 == "player" or player2 == "player"):
                         pass_timer = 60
-                    if player2 != "player":
+                    if player2 != "player"and (player1 == "player" or player2 == "player"):
                         wait_timer = 40
                     event_state = 2
                     pygame.display.set_caption("Pygame Reversi Sample [Player2:White]")
-                elif(input_state == 3):
+                elif input_state == 3:
                     event_state = -1
                     pygame.display.set_caption("Pygame Reversi Sample")
 
             # プレイヤー2のターン
-            elif(event_state == 2):
-                if(player2 == "player"):
+            elif event_state == 2:
+                if player2 == "player":
                     rs.draw_settable_point(2)
                     input_state = rs.standby_player_input(events, 2)
                 else:
                     input_state = rs.standby_computer_input(2, computer_test_player, 0)
-                if(input_state == 1 or input_state == 2):
-                    if(input_state == 2 and (player1 == "player" or player2 == "player")):
+                if input_state == 1 or input_state == 2:
+                    if input_state == 2 and (player1 == "player" or player2 == "player"):
                         pass_timer = 60
-                    if player1 != "player":
+                    if player1 != "player" and (player1 == "player" or player2 == "player"):
                         wait_timer = 40
                     event_state = 1
                     pygame.display.set_caption("Pygame Reversi Sample [Player1:Black]")
-                elif(input_state == 3):
+                elif input_state == 3:
                     event_state = -1
                     pygame.display.set_caption("Pygame Reversi Sample")
 
             # ゲーム終了
-            elif(event_state == -1):
+            elif event_state == -1:
                 # 半透明背景
                 surface.blit(get_pygame_transparent_surface(720, 720, (0, 0, 0, 127)),[0, 0])
                 # 文字列
-                if(rs.count(1) > rs.count(2)):
+                if rs.count(1) > rs.count(2):
                     text = "Black Win! (Black:" + str(rs.count(1)) + ", White:" + str(rs.count(2)) + ")"
-                elif(rs.count(1) < rs.count(2)):
+                elif rs.count(1) < rs.count(2):
                     text = "White Win! (Black:" + str(rs.count(1)) + ", White:" + str(rs.count(2)) + ")"
                 else:
                     text = "Draw (Black:" + str(rs.count(1)) + ", White:" + str(rs.count(2)) + ")"
